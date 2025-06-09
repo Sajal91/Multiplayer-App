880d2a3e3e573edfb3d2349a4d85461ddf494828
Sajal Kapoor <kapoorsajal91@gmail.com>
2025-06-09 15:16:59 +0530

    Update: 1. Production Link Updated

--- Frontend/src/assets/socket.ts
+++ Frontend/src/assets/socket.ts
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
 
-const URL = 'http://localhost:5000/'; // Change if backend is hosted elsewhere
+const URL = 'https://multiplayer-app.onrender.com/'; // Change if backend is hosted elsewhere
 export const socket = io(URL, { autoConnect: false }); 
 
 // https://multiplayer-app.onrender.com/
+// http://localhost:5000/
